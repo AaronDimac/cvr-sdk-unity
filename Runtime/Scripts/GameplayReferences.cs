5b54262fb92a7d3d770452e73da4b3d6f12ef05c
parisacognitive3d <parisa@cognitive3d.com>
2025-06-20 17:22:17 -0400

    Set SDKSupportsEyeTracking to true for OpenXR Vive

--- Runtime/Scripts/GameplayReferences.cs
+++ Runtime/Scripts/GameplayReferences.cs
@@ -63,7 +63,7 @@ namespace Cognitive3D
         {
             get
             {
-#if C3D_SRANIPAL || C3D_VARJOVR || C3D_VARJOXR || C3D_PICOVR || C3D_OMNICEPT
+#if C3D_SRANIPAL || C3D_VARJOVR || C3D_VARJOXR || C3D_PICOVR || C3D_OMNICEPT || COGNITIVE3D_VIVE_OPENXR
                 return true;
 #elif C3D_PICOXR
                 return Unity.XR.PXR.PXR_Manager.Instance.eyeTracking;
