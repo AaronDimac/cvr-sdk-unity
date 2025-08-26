0cdf1a2b46d198411d4a128354f3c3ef6dcda095
parisacognitive3d <parisa@cognitive3d.com>
2025-08-26 19:03:50 -0400

    Updated Social Platform context

--- Editor/Features/FeatureGUIs/SocialPlatformDetailGUI.cs
+++ Editor/Features/FeatureGUIs/SocialPlatformDetailGUI.cs
@@ -27,23 +27,36 @@ namespace Cognitive3D
             GUILayout.EndHorizontal();
 
             GUILayout.Label(
-                "Automatically record Oculus user and app identity data such as App ID, Oculus ID, and Display Name by adding a component that performs an entitlement check using the Oculus platform.",
+                "Automatically record platform user and app identity data (e.g., App ID, User ID, Display Name) by adding a component that performs an entitlement check using the platform’s SDK.",
                 EditorStyles.wordWrappedLabel
             );
 
             EditorGUILayout.Space(10);
 
-            GUILayout.Label("1. Prepare Your Oculus App (Meta Developer Portal)", EditorCore.styles.FeatureTitle);
+            GUILayout.Label("1. Prepare Your App (Developer Portal)", EditorCore.styles.FeatureTitle);
 
             GUILayout.Label(
-                "Set up your Oculus App ID on the Developer Dashboard, enable user data permissions, publish the app, and add the App ID in Unity under Oculus > Platform > Edit Settings.",
+                "Set up your app ID on the platform's developer dashboard, enable the necessary user data permissions, publish the app.",
                 EditorStyles.wordWrappedLabel
             );
+
+#if C3D_OCULUS
+            EditorGUILayout.Space(5);
+            
+            EditorGUILayout.HelpBox(
+                "To enable this feature for Meta (Oculus):\n" +
+                "• Set up your Oculus App ID in the Meta Developer Dashboard.\n" +
+                "• Enable the necessary user data permissions.\n" +
+                "• Publish your app.\n" +
+                "• Add the App ID in Unity under Oculus > Platform > Edit Settings.",
+                MessageType.Info
+            );
+#endif
 
             EditorGUILayout.Space(10);
 
             GUILayout.Label("2. Add to Cognitive3D_Manager prefab", EditorCore.styles.FeatureTitle);
-            GUILayout.Label("Adds the Social Platform component to the Cognitive3D_Manager prefab to record Oculus user data.", EditorStyles.wordWrappedLabel);
+            GUILayout.Label("Adds the Social Platform component to the Cognitive3D_Manager prefab to record platform-specific user data.", EditorStyles.wordWrappedLabel);
 
             var btnLabel = FeatureLibrary.TryGetComponent<Cognitive3D.Components.SocialPlatform>() ? "Remove Social Platform" : "Add Social Platform";
             if (GUILayout.Button(btnLabel, GUILayout.Height(30)))
